e6b081044772e5b470a6a61500e12fb7842e7314
Anthony Ulloa M <tonyum@estudiantec.cr>
2019-10-28 15:19:23 -0600

    Cambios menores

--- ProjectVs/ProjectVs/main.c
+++ ProjectVs/ProjectVs/main.c
@@ -2,8 +2,20 @@
 #include <stdbool.h>
 #include "matroids.h"
 
+bool isMultFive(int pNumber) {
+	return (pNumber % 10 == 5 || pNumber % 10 == 0) ? true : false;
+}
+
 bool isEven(int pNumber) {
 	return pNumber % 2 == 0;
+}
+
+bool isUneven(int pNumber) {
+	return pNumber % 2 != 0;
+}
+
+bool isPalindrome() {
+	return true;
 }
 
 void* swap(void* vp1, void* vp2, const int size) { // Still with problems, see the link https://www.bogotobogo.com/cplusplus/pointers2_voidpointers_arrays.php
@@ -17,47 +29,86 @@ void* swap(void* vp1, void* vp2, const int size) { // Still with problems, see t
 int main() {
 
 	// -------------- Test #1 -------------- //
+	// -------------- Matroid 1.1 -------------- //
 	struct matroid test1;
-	int numbers1[] = { 51, 23, 36, 74, 89 };
-	int result[5] = { -1 };
+	int numbers1[] = { 55, 20, 35, 75, 89 };
+	int resultN1[sizeof(numbers1) / sizeof(numbers1[0])] = { -1 };
 	test1.MS = &numbers1;
-	test1.MI = &result;
-	test1.size = sizeof(numbers1) / sizeof(numbers1[0]);;
+	test1.MI = &resultN1;
+	test1.size = sizeof(numbers1) / sizeof(numbers1[0]);
+	test1.func = isMultFive;
 
 	// -------------- Pointer VOID tests -------------- //
 	int(*p)[] = &numbers1;
 	//printf("%p \n", &numbers1);
 
-	// -------------- Test #2 -------------- //
+	// -------------- Matroid 1.2 -------------- //
 	struct matroid test2;
 	int numbers2[] = { 114, 253, 625, 789, 800, 194, 310, 811, 212 };
-	int result2[9] = { -1 };
+	int resultN2[sizeof(numbers2) / sizeof(numbers2[0])] = { -1 };
 	test2.MS = &numbers2;
-	test2.MI = &result;
+	test2.MI = &resultN2;
 	test2.size = sizeof(numbers2) / sizeof(numbers2[0]);
 	test2.func = isEven;
 
-	// -------------- Test #3 -------------- //
+	// -------------- Matroid 1.3 -------------- //
 	struct matroid test3;
-	char letters1[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
-	char resultL1[6] = { -1 };
-	test3.MS = &letters1;
-	test3.MI = &resultL1;
-	test3.size = sizeof(letters1) / sizeof(letters1[0]);
-	test3.func = swap(&letters1[0], &letters1[0], sizeof(char));
+	int numbers3[] = { 4, 7, 6, 9, 0, 1 };
+	int resultN3[sizeof(numbers3) / sizeof(numbers3[0])] = { -1 };
+	test3.MS = &numbers3;
+	test3.MI = &resultN3;
+	test3.size = sizeof(numbers3) / sizeof(numbers3[0]);
+	test3.func = isUneven;
 
-	// -------------- Test #4 -------------- //
+	// -------------- Test #2 -------------- //
+	// -------------- Matroid 2.1 -------------- //
 	struct matroid test4;
-	char letters2[] = { 'z', 'y', 'x', 'w', 'v', 'u', 't' };
-	char resultL2[7] = { -1 };
-	test4.MS = &letters2;
-	test4.MI = &resultL2;
-	test4.size = sizeof(letters2) / sizeof(letters2[0]);
-	test4.func = swap(&letters2[0], &letters2[0], sizeof(char));
+	char letters1[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+	char resultL1[sizeof(letters1) / sizeof(letters1[0])] = { -1 };
+	test4.MS = &letters1;
+	test4.MI = &resultL1;
+	test4.size = sizeof(letters1) / sizeof(letters1[0]);
+	test4.func = swap(&letters1[0], &letters1[1], sizeof(char));
+
+	// -------------- Matroid 2.2 -------------- //
+	struct matroid test5;
+	char letters2[] = { 'r', 'a', 'c', 'e', 'c', 'a', 'r' };
+	char resultL2[sizeof(letters2) / sizeof(letters2[0])] = { -1 };
+	test5.MS = &letters2;
+	test5.MI = &resultL2;
+	test5.size = sizeof(letters2) / sizeof(letters2[0]);
+	test5.func = isPalindrome;
+
+	// -------------- Matroid 2.3 -------------- //
+	struct matroid test6;
+	char letters3[] = { 'k', 'u', 's', 'h' };
+	char resultL3[sizeof(letters3) / sizeof(letters3[0])] = { -1 };
+	test6.MS = &letters3;
+	test6.MI = &resultL3;
+	test6.size = sizeof(letters3) / sizeof(letters3[0]);
+	test6.func = NULL;
+
+	// -------------- Matroid 2.4 -------------- //
+	struct matroid test7;
+	char letters4[] = { 'f', 'o', 'r' };
+	char resultL4[sizeof(letters4) / sizeof(letters4[0])] = { -1 };
+	test7.MS = &letters4;
+	test7.MI = &resultL4;
+	test7.size = sizeof(letters4) / sizeof(letters4[0]);
+	test7.func = NULL;
+
+	// -------------- Matroid 2.5 -------------- //
+	struct matroid test8;
+	char letters5[] = { 'm', 'e' };
+	char resultL5[sizeof(letters5) / sizeof(letters5[0])] = { -1 };
+	test8.MS = &letters5;
+	test8.MI = &resultL5;
+	test8.size = sizeof(letters5) / sizeof(letters5[0]);
+	test8.func = NULL;
 
 	// -------------- Matroids -------------- //
-	struct matroid matroids[2] = { test1, test2 };
-	//struct matroid matroids2[2] = { test3, test4 };
+	struct matroid matroids[3] = { test1, test2, test3 };
+	//struct matroid matroids2[5] = { test4, test5, test6, test7, test8 };
 	//printf("%p \n",&matroids);
 
 	// -------------- Obtener el arreglo -------------- //
@@ -75,4 +126,4 @@ int main() {
 
 	system("PAUSE");
 	return 0;
-}
+}}
--- ProjectVs/ProjectVs/matroids.c
+++ ProjectVs/ProjectVs/matroids.c
@@ -18,43 +18,45 @@ void evaluateMatroids(struct matroid matroidsList[], int size) {
 
 void evaluate(struct matroid matroidsList[], int size) {
 	struct matroid test;
-#pragma omp parallel sections num_threads(size)
+
+	// -------------- Method #1 -------------- //
+	int i = 0, j = 0, arrayNumber = 0;
+#pragma omp parallel
 	{
-		// -------------- Method #1 -------------- //
-#pragma omp parallel section
-		{
-			for (int i = 0; i < size; i++) {
-				printf("We´re in section 1, thread %d \n", omp_get_thread_num());
-				test = matroidsList[i];
-				char* buffer = (char*)malloc(sizeof(char) * size);
-				buffer = test.MS;
-				for (int j = 0; j < test.size; j++) {
-					printf("The number of position %d is: %d \n", j, ((int*)buffer)[j]);
-				}
-				printf("\n");
+#pragma omp for
+		for (i = 0; i < size; i++) {
+			printf("We are in section 1, thread %d \n", omp_get_thread_num());
+			test = matroidsList[i];
+			char* buffer = (char*)malloc(sizeof(char) * size);
+			buffer = test.MS;
+			for (j = 0; j < test.size; j++) {
+				printf("The number of position %d is: %d \n", arrayNumber, ((int*)buffer)[j]);
 			}
-		}
-
-		// -------------- Method #2 -------------- //
-#pragma omp parallel section 
-		{
-			for (int i = 0; i < size; i++) {
-				printf("We´re in section 2, thread %d \n", omp_get_thread_num());
-				test = matroidsList[i];
-				char* buffer = (char*)malloc(sizeof(char) * size);
-				buffer = test.MS;
-				//Curious behavior when changing the print, this is due to the type of buffer thinking about other solutions if you want to use how it is
-
-				for (int j = 0; j < test.size; j++) {
-					printf("The number of position %d is: %d \n", j, buffer[j]);
-				}
-				printf("\n");
-			}
+			printf("\n");
+			arrayNumber++;
 		}
 	}
-	//The change was made to work with N elements
-	//In char buffer, we don't really care if it's character or not.
-	//All we want is to have enough bytes to store something with size byte.
-	//In other words, we need storage.
-	//Check https://www.bogotobogo.com/cplusplus/pointers2_voidpointers_arrays.php
+
+	// -------------- Method #2 -------------- //
+		//int k = 0, h = 0, arrayNumber2 = 0;
+		//#pragma omp parallel
+		//{
+		//	#pragma omp for
+		//	for (k = 0; k < size; k++) {
+		//		printf("We´re in section 2, thread %d \n", omp_get_thread_num());
+		//		test = matroidsList[k];
+		//		char* buffer = (char*)malloc(sizeof(char) * size);
+		//		buffer = test.MS;
+		//		//Curious behavior when changing the print, this is due to the type of buffer thinking about other solutions if you want to use how it is
+		//		for (h = 0; h < test.size; h++) {
+		//			printf("The number of position %d is: %d \n", arrayNumber2, buffer[h]);
+		//		}
+		//		printf("\n");
+		//	}
+		//}
+		//The change was made to work with N elements
+		//In char buffer, we don't really care if it's character or not.
+		//All we want is to have enough bytes to store something with size byte.
+		//In other words, we need storage.
+		//Check https://www.bogotobogo.com/cplusplus/pointers2_voidpointers_arrays.php
 }
