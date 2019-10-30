d391f31a556270db5bc859ae71dd7ac5d67fb589
Anthony Ulloa M <tonyum@estudiantec.cr>
2019-10-30 00:44:58 -0600

    Correccion menor y se agrego parte B

--- ProjectVs/ProjectVs/main.c
+++ ProjectVs/ProjectVs/main.c
@@ -1,180 +1,128 @@
 #include <stdio.h>
+//#include <stdlib.h>
 #include <stdbool.h>
 #include "matroids.h"
 
-bool isMultFive(int pNumber) {
-	return (pNumber % 10 == 5 || pNumber % 10 == 0) ? true : false;
-}
-
-bool isEven(void *pNumber) {
+bool isMultFive(void* pNumber) {
 	int number = *(int*)(pNumber);
-	if (number % 2 == 0) {
-		printf("%d es par \n", number);
+	if (number % 10 == 5 || number % 10 == 0) {
+		printf("The number %d is multiple of five \n", number);
 		return true;
 	}
 	else {
-		printf("%d es impar \n", number);
+		printf("The number %d is not multiple of five \n", number);
 		return false;
 	}
-	//return number % 2 == 0;
+	// return (number % 10 == 5 || number % 10 == 0) ? true : false;
 }
 
-bool isUneven(int pNumber) {
-	return pNumber % 2 != 0;
+bool isEven(void* pNumber) {
+	int number = *(int*)(pNumber);
+	if (number % 2 == 0) {
+		printf("The number %d is even \n", number);
+		return true;
+	}
+	else {
+		printf("The number %d is not even \n", number);
+		return false;
+	}
 }
 
-bool isPalindrome() {
-	return true;
+bool isBlackJack(void* pNumber) {
+	int firstCard = rand() % 4 + 13;
+	int secondCard = *(int*)(pNumber);
+
+	if (firstCard + secondCard == 21) {
+		// printf("Winner, You got BLACKJACK with %d and %d \n", firstCard, secondCard);
+		return true;
+	}
+	else if (firstCard + secondCard < 21) {
+		// printf("You did not get BLACKJACK either lose with %d and %d \n", firstCard, secondCard);
+		return true;
+	}
+	else {
+		// printf("Loser, You did not get BLACKJACK with %d and %d \n", firstCard, secondCard);
+		return false;
+	}
 }
 
-bool isUpper(void *pLetter) {
+bool isUpper(void* pLetter) {
 	char letter = *((char*)(pLetter));
 	int num = (int)letter;
 	if (num >= 60 && num <= 90) {
-		printf("%c es mayuscula \n", letter);
+		printf("The letter %c is a capital letter \n", letter);
 		return true;
 	}
-	printf("%c no es mayuscula \n", letter);
+	printf("The letter %c is not a capital letter \n", letter);
 	return false;
 }
 
-void* swap(void* vp1, void* vp2, const int size) { // Still with problems, see the link https://www.bogotobogo.com/cplusplus/pointers2_voidpointers_arrays.php
-	char* buffer = (char*)malloc(sizeof(char) * size);
-	memcpy(buffer, vp1, size);
-	memcpy(vp1, vp2, size);
-	memcpy(vp2, buffer, size);
-	free(buffer);
+void printMI(void* (*MI)[], int size) {
+	for (int printN = 0; printN < size; printN++) {
+		int number = *(int*)((*MI)[printN]);
+		(printN + 1 < size) ? printf("%d - ", number) : printf("%d \n", number);
+	}
 }
 
-void printMI(void* (*MI)[3], int size) {
-	for (int i = 0; i < size; i++) {
-		int number = *(int*)((*MI)[i]);
-		printf("%d - ", number);
+void printMI2(void* (*MI)[], int size) {
+	for (int printL = 0; printL < size; printL++) {
+		char letter = *(char*)((*MI)[printL]);
+		(printL + 1 < size) ? printf("%c - ", letter) : printf("%c \n", letter);
 	}
-	printf("\n");
-}
-
-void printMI2(void* (*MI)[3], int size) {
-	for (int i = 0; i < size; i++) {
-		char number = *(char*)((*MI)[i]);
-		printf("%c - ", number);
-	}
-	printf("\n");
 }
 
 int main() {
-	void* MS[3] = { &(int) { 56 }, &(int){57}, &(int){58} };
-	void* MI[3] = { &(int) { 0 }, &(int) { 0 }, &(int) { 0 } };
+	// FIRST PART
+	void* MSA1[4] = { &(char) { 'E' }, &(char) { 'a' }, &(char) { 'S' }, &(char) { 'y' } };
+	void* MIA1[4] = { &(char) { ' ' }, &(char) { ' ' }, &(char) { ' ' }, &(char) { ' ' } };
 
-	void* MS2[3] = { &(int) { 61 }, &(int){60}, &(int){59} };
-	void* MI2[3] = { &(int) { 0 }, &(int) { 0 }, &(int) { 0 } };
+	void* MSA2[3] = { &(int) { 65 }, &(int) { 46 }, &(int) { 83 } };
+	void* MIA2[3] = { &(int) { 0 }, &(int) { 0 }, &(int) { 0 } };
 
-	void* MS3[3] = { &(char) { 'A' }, &(char) { 'C' }, &(char) { 'd' } };
-	void* MI3[3] = { &(char) { ' ' }, &(char) { ' ' }, &(char) { ' ' } };
+	void* MSA3[5] = { &(int) { 70 }, &(int) { 85 }, &(int) { 117 }, &(int) { 67 }, &(int) { 75 } };
+	void* MIA3[5] = { &(int) { 0 }, &(int) { 0 }, &(int) { 0 }, &(int) { 0 }, &(int) { 0 } };
 
-	void* Matroids[3][4] = { {&MS, &MI, &(int) { 3 }, &isEven}, {MS2, MI2, &(int) { 3 }, &isEven}, {&MS3, &MI3, &(int) { 3 }, &isUpper} };
+	void* MatroidsA[3][4] = { {&MSA1, &MIA1, &(int) { 4 }, &isUpper}, {&MSA2, &MIA2, &(int) { 3 }, &isEven}, {&MSA3, &MIA3, &(int) { 5 }, &isMultFive} };
+
+	// SECOND PART
+	void* MSB1[4] = { &(int) { 6 }, &(int) { 9 }, &(int) { 5 }, &(int) { 7 } };
+	void* MIB1[4] = { &(int) { 0 }, &(int) { 0 }, &(int) { 0 }, &(int) { 0 } };
+
+	void* MSB2[3] = { &(int) { 5 }, &(int) { 6 }, &(int) { 7 } };
+	void* MIB2[3] = { &(int) { 0 }, &(int) { 0 }, &(int) { 0 } };
+
+	void* MSB3[4] = { &(int) { 7 }, &(int) { 5 }, &(int) { 10 }, &(int) { 6 } };
+	void* MIB3[4] = { &(int) { 0 }, &(int) { 0 }, &(int) { 0 }, &(int) { 0 } };
+
+	void* MSB4[5] = { &(int) { 11 }, &(int) { 8 }, &(int) { 6 }, &(int) { 9 }, &(int) { 5 } };
+	void* MIB4[5] = { &(int) { 0 }, &(int) { 0 }, &(int) { 0 }, &(int) { 0 }, &(int) { 0 } };
+
+	void* MSB5[6] = { &(int) { 5 }, &(int) { 9 }, &(int) { 8 }, &(int) { 10 }, &(int) { 6 }, &(int) { 7 } };
+	void* MIB5[6] = { &(int) { 0 }, &(int) { 0 }, &(int) { 0 }, &(int) { 0 }, &(int) { 0 }, &(int) { 0 } };
+
+	void* MatroidsB[5][4] = { {&MSB1, &MIB1, &(int) { 4 }, &isBlackJack}, {&MSB2, &MIB2, &(int) { 3 }, &isBlackJack}, {&MSB3, &MIB3, &(int) { 4 }, &isBlackJack}
+																		,{&MSB4, &MIB4, &(int) { 5 }, &isBlackJack}, {&MSB5, &MIB5, &(int) { 6 }, &isBlackJack} };
 
 	//void* Matroids[4] = { &(void*) { MS }, &(void*){MI},&(void*) { MS2 }, &(void*){MI2} };
 
-	evaluateMatroids(&Matroids, sizeof(Matroids) / sizeof(Matroids[0]));
 	// Resultados
 	// Matroid1
 	//printf("%d \n", *(int*)MI[0]);
-	printMI(&MI, 3);
-	printMI(&MI2, 3);
-	printMI2(&MI3, 3);
-	/*
-	// -------------- Test #1 -------------- //
-	struct matroid test1;
-	int numbers1[] = { 50, 23, 36, 74, 89 };
-	int result[5] = { -1 };
-	test1.MS = &numbers1;
-	test1.MI = &result;
-	test1.size = sizeof(numbers1) / sizeof(numbers1[0]);;
-	test1.func = &isEven;
 
-	// -------------- Test #2 -------------- //
-	struct matroid test2;
-	int numbers2[] = { 114, 253, 625, 789, 800, 194, 310, 811, 212 };
-	int result2[9] = { -1 };
-	test2.MS = &numbers2;
-	test2.MI = &result2;
-	test2.size = sizeof(numbers2) / sizeof(numbers2[0]);
-	test2.func = isEven;
+	// --------------- PART A EVALUATION --------------- //
+	evaluateMatroidsA(&MatroidsA, sizeof(MatroidsA) / sizeof(MatroidsA[0]));
+	printMI2(&MIA1, 4);
+	printMI(&MIA2, 3);
+	printMI(&MIA3, 5);
+	printf("----------------- \n");
+	// --------------- PART B INTERSECTIONS --------------- //
+	evaluateMatroidsB(&MatroidsB, sizeof(MatroidsB) / sizeof(MatroidsB[0]));
+	printMI(&MIB1, 4);
+	printMI(&MIB2, 3);
+	printMI(&MIB3, 4);
+	printMI(&MIB4, 5);
+	printMI(&MIB5, 6);
 
-	// -------------- Matroid 1.3 -------------- //
-	struct matroid test3;
-	int numbers3[] = { 4, 7, 6, 9, 0, 1 };
-	int resultN3[sizeof(numbers3) / sizeof(numbers3[0])] = { -1 };
-	test3.MS = &numbers3;
-	test3.MI = &resultN3;
-	test3.size = sizeof(numbers3) / sizeof(numbers3[0]);
-	test3.func = isUneven;
-
-	// -------------- Test #2 -------------- //
-	// -------------- Matroid 2.1 -------------- //
-	struct matroid test4;
-	char letters1[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
-	char resultL1[sizeof(letters1) / sizeof(letters1[0])] = { -1 };
-	test4.MS = &letters1;
-	test4.MI = &resultL1;
-	test4.size = sizeof(letters1) / sizeof(letters1[0]);
-	test4.func = swap(&letters1[0], &letters1[1], sizeof(char));
-
-	// -------------- Matroid 2.2 -------------- //
-	struct matroid test5;
-	char letters2[] = { 'r', 'a', 'c', 'e', 'c', 'a', 'r' };
-	char resultL2[sizeof(letters2) / sizeof(letters2[0])] = { -1 };
-	test5.MS = &letters2;
-	test5.MI = &resultL2;
-	test5.size = sizeof(letters2) / sizeof(letters2[0]);
-	test5.func = isPalindrome;
-
-	// -------------- Matroid 2.3 -------------- //
-	struct matroid test6;
-	char letters3[] = { 'k', 'u', 's', 'h' };
-	char resultL3[sizeof(letters3) / sizeof(letters3[0])] = { -1 };
-	test6.MS = &letters3;
-	test6.MI = &resultL3;
-	test6.size = sizeof(letters3) / sizeof(letters3[0]);
-	test6.func = NULL;
-
-	// -------------- Matroid 2.4 -------------- //
-	struct matroid test7;
-	char letters4[] = { 'f', 'o', 'r' };
-	char resultL4[sizeof(letters4) / sizeof(letters4[0])] = { -1 };
-	test7.MS = &letters4;
-	test7.MI = &resultL4;
-	test7.size = sizeof(letters4) / sizeof(letters4[0]);
-	test7.func = NULL;
-
-	// -------------- Matroid 2.5 -------------- //
-	struct matroid test8;
-	char letters5[] = { 'm', 'e' };
-	char resultL5[sizeof(letters5) / sizeof(letters5[0])] = { -1 };
-	test8.MS = &letters5;
-	test8.MI = &resultL5;
-	test8.size = sizeof(letters5) / sizeof(letters5[0]);
-	test8.func = NULL;
-
-	// -------------- Matroids -------------- //
-	struct matroid matroids[3] = { test1, test2, test3 };
-	//struct matroid matroids2[5] = { test4, test5, test6, test7, test8 };
-	//printf("%p \n",&matroids);
-
-	// -------------- Obtener el arreglo -------------- //
-	int(*testing)[5] = test1.MS;
-
-	// -------------- Evaluate -------------- //
-	//evaluateMatroids(matroids, sizeof(matroids) / sizeof(matroids[0]));
-	//evaluate(matroids, sizeof(matroids) / sizeof(matroids[0]));
-	//evaluate(matroids2, sizeof(matroids2) / sizeof(matroids2[0])); //To use this you must change the print from %d to %s
-
-	// -------------- Pointer VOID tests -------------- //
-	//int k[] = { 4, 5, 6, 7, 8 };
-	//void* ptr = &k[1];
-	//printf("%d \n", *(int*)ptr); // Shows number 5
-	*/
 	system("PAUSE");
 	return 0;
 }
--- ProjectVs/ProjectVs/matroids.c
+++ ProjectVs/ProjectVs/matroids.c
@@ -1,70 +1,48 @@
 #include "matroids.h"
 
-void evaluateMatroids(void* (*Matroids)[2][4], int size) {
-	//void* Matroid;
-	int i, amountOfElements;
-#pragma omp parallel for private(i, amountOfElements)
-	for (i = 0; i < size; i++) {
-		printf("i = %d threadId = %d \n", i, omp_get_thread_num());
-		void* (*Matroid)[4] = (*Matroids)[i];	// Se saca la matroid
-		int arraySize = *((int*)((*Matroid)[2]));	// Se saca el tamaño del conjunto S
-		void* (*MS)[] = (*Matroid)[0];	// Se saca el conjunto S
-		bool (*func)(void*) = (*Matroid)[3];	// Se saca la funcion para evaluar los elementos del conjunto S
-		//void** MI = malloc(arraySize * sizeof(void*));
-		void* (*MI)[] = (*Matroid)[1];
+void evaluateMatroidsA(void* (*Matroids)[2][4], int size) {
+	int amountOfMatroids, amountOfElements;
 
-		//printf("%d \n", sizeof(void*));
+#pragma omp parallel for private(amountOfMatroids, amountOfElements)
+	for (amountOfMatroids = 0; amountOfMatroids < size; amountOfMatroids++) {
+		printf("amountOfMatroids = %d threadId = %d \n", amountOfMatroids, omp_get_thread_num());
+
+		void* (*Matroid)[4] = (*Matroids)[amountOfMatroids];	// The matroid is obtained
+		int arraySize = *((int*)((*Matroid)[2]));				// The size of the set S is obtained
+		void* (*MS)[] = (*Matroid)[0];							// The set S is obtained
+		bool (*func)(void*) = (*Matroid)[3];					// The function is obtained to evaluate the elements of the set S
+
+		void* (*MI)[] = (*Matroid)[1];
 		for (amountOfElements = 0; amountOfElements < arraySize; amountOfElements++) {
-			// Se pasa el elemento a la funcion
+			// The element is passed to the function
 			bool result = (*func)((*MS)[amountOfElements]);
 			if (result) {
 				(*MI)[amountOfElements] = (*MS)[amountOfElements];
 			}
 		}
 	}
+	printf("----------------- \n");
 }
 
-void evaluate(struct matroid matroidsList[], int size) {
-	struct matroid test;
+void evaluateMatroidsB(void* (*Matroids)[5][4], int size) {
+	for (int amountOfMatroids = 0; amountOfMatroids < size; amountOfMatroids++) {
 
-	// -------------- Method #1 -------------- //
-	int i = 0, j = 0, arrayNumber = 0;
-#pragma omp parallel
-	{
-#pragma omp for
-		for (i = 0; i < size; i++) {
-			printf("We are in section 1, thread %d \n", omp_get_thread_num());
-			test = matroidsList[i];
-			char* buffer = (char*)malloc(sizeof(char) * size);
-			buffer = test.MS;
-			for (j = 0; j < test.size; j++) {
-				printf("The number of position %d is: %d \n", arrayNumber, ((int*)buffer)[j]);
+		void* (*Matroid)[4] = (*Matroids)[amountOfMatroids];	// The matroid is obtained
+		int arraySize = *((int*)((*Matroid)[2]));				// The size of the set S is obtained
+		void* (*MS)[] = (*Matroid)[0];							// The set S is obtained
+		bool (*func)(void*) = (*Matroid)[3];					// The function is obtained to evaluate the elements of the set S
+
+		void* (*MI)[] = (*Matroid)[1];
+		for (int amountOfElements = 0; amountOfElements < arraySize; amountOfElements++) {
+			bool result = (*func)((*MS)[amountOfElements]);		// The element is passed to the function
+			if (result) {
+				(*MI)[amountOfElements] = (*MS)[amountOfElements];
 			}
-			printf("\n");
-			arrayNumber++;
 		}
 	}
-
-	// -------------- Method #2 -------------- //
-		//int k = 0, h = 0, arrayNumber2 = 0;
-		//#pragma omp parallel
-		//{
-		//	#pragma omp for
-		//	for (k = 0; k < size; k++) {
-		//		printf("We´re in section 2, thread %d \n", omp_get_thread_num());
-		//		test = matroidsList[k];
-		//		char* buffer = (char*)malloc(sizeof(char) * size);
-		//		buffer = test.MS;
-		//		//Curious behavior when changing the print, this is due to the type of buffer thinking about other solutions if you want to use how it is
-		//		for (h = 0; h < test.size; h++) {
-		//			printf("The number of position %d is: %d \n", arrayNumber2, buffer[h]);
-		//		}
-		//		printf("\n");
-		//	}
-		//}
-		//The change was made to work with N elements
-		//In char buffer, we don't really care if it's character or not.
-		//All we want is to have enough bytes to store something with size byte.
-		//In other words, we need storage.
-		//Check https://www.bogotobogo.com/cplusplus/pointers2_voidpointers_arrays.php
+	//#pragma omp parallel for private(amountOfMatroids, amountOfElements)
+	//	for (int intersections = 0; intersections < size; intersections++) {
+	//		printf("amountOfMatroids = %d threadId = %d \n", amountOfMatroids, omp_get_thread_num());
+	//	}
+	printf("----------------- \n");
 }
